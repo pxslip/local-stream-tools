a9c23f1caf2409cc305a493d855071c7d609404f
pxslip <408277+pxslip@users.noreply.github.com>
2021-01-04 23:38:25 -0500

    Clean up the config file

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,12 @@
 module.exports = {
-  // pages: {
-  //   app: {
-  //     entry: 'src/renderer.js',
-  //     template: 'src/renderer/public/index.html',
-  //   },
-  // },
   pluginOptions: {
     electronBuilder: {
       preload: 'src/renderer/preload.js',
       customFileProtocol: 'lst://./',
       mainProcessFile: 'src/main.js',
       rendererProcessFile: 'src/renderer.js',
+      externals: ['keytar'],
+      mainProcessWatch: ['src/main/**/*.js', 'src/lib/**/*.js'],
     },
   },
 };
