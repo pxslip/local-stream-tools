e0ee9f917c3e74b8412aca41819ee2178fb17a0a
pxslip <408277+pxslip@users.noreply.github.com>
2021-02-20 15:56:41 -0500

    This will now behave like an input to vue

--- src/renderer/components/inputs/UnderlineInput.vue
+++ src/renderer/components/inputs/UnderlineInput.vue
@@ -1,22 +1,27 @@
 <template>
   <label :class="$attrs.class">
     <slot></slot>
-    <input :type="type" class="underline" :value="value" @input="$emit('update:value', $event.target.value)" />
+    <input
+      :type="type"
+      class="underline"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
   </label>
 </template>
 
 <script>
 export default {
   props: {
-    value: [String, Number],
+    modelValue: [String, Number],
     type: String,
   },
-  emits: ['update:value'],
+  emits: ['update:modelValue'],
 };
 </script>
 
 <style lang="postcss">
 input.underline {
-  @apply bg-transparent block border-0 border-b-2 border-gray-400 focus:border-gray-200 focus:ring-0 w-full;
+  @apply bg-transparent block border-0 border-b-2 border-gray-400 focus:border-gray-200 focus:ring-0 w-full no-underline;
 }
 </style>
