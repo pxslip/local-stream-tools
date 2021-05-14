7932ea47b208ccc3ac91a7e2aeba2d707974ce22
pxslip <408277+pxslip@users.noreply.github.com>
2021-05-13 21:39:12 -0400

    Pulled out the left nav to a component

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,12 +1,6 @@
 <template>
   <div class="lg:container grid gap-1 grid-cols-6">
-    <nav id="nav" class="col-span-1 text-gray-100">
-      <router-link class="block" to="/">Home</router-link>
-      <router-link class="block" to="/chat">Chat</router-link>
-      <router-link class="block" to="/commands">Commands</router-link>
-      <router-link class="block" to="/settings">Settings</router-link>
-      <router-link class="block" to="/about">About</router-link>
-    </nav>
+    <left-nav />
     <main class="col-span-5">
       <router-view />
     </main>
@@ -14,7 +8,9 @@
 </template>
 
 <script>
+import LeftNav from './components/LeftNav';
 export default {
+  components: { LeftNav },
   created() {
     let hash = window.location.hash.replace('#', '');
     if (hash) {
---
+++ src/renderer/components/LeftNav.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav id="nav" class="col-span-1 text-gray-100">
+    <router-link class="block" to="/">Home</router-link>
+    <router-link class="block" to="/chat">Chat</router-link>
+    <router-link class="block" to="/commands">Commands</router-link>
+    <router-link class="block" to="/settings">Settings</router-link>
+    <router-link class="block" to="/about">About</router-link>
+    <button>Connect to Twitch</button>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
